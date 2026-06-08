0ecac0eb284a4f4914c3b6be5f5da47946257260
Deku-foryou <reyhanmagribi57@gmail.com>
2026-06-08 16:53:35 +0700

    Add data user

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -17,4 +17,42 @@ class TugasController extends Controller
         );
         return view('admin/tugas/index', $data);
     }
+
+    public function create()
+    {
+        $data = array(
+            'title' => 'tambah Data Tugas',
+            'menuAdminTugas' => 'active',
+            'user' => User::where('jabatan', 'Karyawan')->where('is_tugas', false)->get(),
+
+        );
+        return view('admin/tugas/create', $data);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'tugas' => 'required',
+            'tanggal_mulai' => 'required',
+            'tanggal_selesai' => 'required',
+        ], [
+            'user_id.required' => 'Nama Tidak Boleh Kosong',
+            'tugas.required' => 'Tugas Tidak Boleh Kosong',
+            'tanggal_mulai.required' => 'Tanggal Mulai Tidak Boleh Kosong',
+            'tanggal_selesai.required' => 'Tanggal Selesai Tidak Boleh Kosong',
+        ]);
+
+        $user = User::findOrFail($request->user_id);
+        $tugas = new Tugas;
+        $tugas ->user_id = $request->user_id;
+        $tugas ->tugas = $request->tugas;
+        $tugas ->tanggal_mulai = $request->tanggal_mulai;
+        $tugas ->tanggal_selesai = $request->tanggal_selesai;
+        $tugas->save();
+
+        $user->is_tugas = true;
+        $user->save();
+
+        return redirect()->route('tugas')->with('success', 'Data Berhasil Ditambahkan');
+    }
 }
