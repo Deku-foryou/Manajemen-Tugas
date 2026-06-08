159b38ff53a9823cb7cf8d217ec6cb3a60edb23f
Deku-foryou <reyhanmagribi57@gmail.com>
2026-06-08 20:48:38 +0700

    edit & delete tugas

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -44,15 +44,57 @@ class TugasController extends Controller
 
         $user = User::findOrFail($request->user_id);
         $tugas = new Tugas;
-        $tugas ->user_id = $request->user_id;
-        $tugas ->tugas = $request->tugas;
-        $tugas ->tanggal_mulai = $request->tanggal_mulai;
-        $tugas ->tanggal_selesai = $request->tanggal_selesai;
+        $tugas->user_id = $request->user_id;
+        $tugas->tugas = $request->tugas;
+        $tugas->tanggal_mulai = $request->tanggal_mulai;
+        $tugas->tanggal_selesai = $request->tanggal_selesai;
         $tugas->save();
 
         $user->is_tugas = true;
         $user->save();
 
         return redirect()->route('tugas')->with('success', 'Data Berhasil Ditambahkan');
+    }
+
+    public function edit($id)
+    {
+        $data = array(
+            'title' => 'Edit Data Tugas',
+            'menuAdminTugas' => 'active',
+            'tugas' => Tugas::with('user')->findOrFail($id),
+        );
+        return view('admin/tugas/update', $data);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'tugas' => 'required',
+            'tanggal_mulai' => 'required',
+            'tanggal_selesai' => 'required',
+        ], [
+            'tugas.required' => 'Tugas Tidak Boleh Kosong',
+            'tanggal_mulai.required' => 'Tanggal Mulai Tidak Boleh Kosong',
+            'tanggal_selesai.required' => 'Tanggal Selesai Tidak Boleh Kosong',
+        ]);
+
+        $tugas = Tugas::findOrFail($id);
+        $tugas->tugas = $request->tugas;
+        $tugas->tanggal_mulai = $request->tanggal_mulai;
+        $tugas->tanggal_selesai = $request->tanggal_selesai;
+        $tugas->save();
+
+        return redirect()->route('tugas')->with('success', 'Data Berhasil Di Edit');
+    }
+    public function destroy($id)
+    {
+        $tugas = Tugas::findOrFail($id);
+        $tugas->delete();
+        $user = User::where('id',$tugas->user_id)
+        ->first();
+        $user->is_tugas = false;
+        $user->save();
+
+        return redirect()->route('tugas')->with('success', 'Data Berhasil Di Hapus');
     }
 }
